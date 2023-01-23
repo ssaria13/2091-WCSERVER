41dadee3cb3e11ccdbec5d710eb9955c458d2fa6
ssaria13 <123181884+ssaria13@users.noreply.github.com>
2023-01-23 11:43:15 +0800

    Add files via upload
    Initial Prelims Files (2nd-3rd week)

---
+++ Prelims/node-example.js
@@ -0,0 +1,15 @@
+// Name: Aubrey Lisber A. Castro
+// Module: 3
+// Week 2 and 3: Basics of NodeJS
+
+//First program
+console.log("Welcome to WCSERVER!");
+console.log("I love Enhypen <3")
+
+
+//First Function
+function Display(x) {
+    console.log(x);
+}
+
+Display(100);
